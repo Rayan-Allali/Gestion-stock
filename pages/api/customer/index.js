d7cb5dc54e184da42d1a0c98cb4b7ddc4424568e
Rayan-Allali <108684980+Rayan-Allali@users.noreply.github.com>
2023-01-12 22:09:52 +0100

    customer and supplier routes

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,7 +1,8 @@
-import {getAllHandler} from '../../../controllers/customerControllers.js'
+import {getAllHandler,postHandler} from '../../../controllers/customerControllers.js'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') return getAllHandler(req, res);
+  if(req.method === 'POST') return postHandler(req, res);
   res
     .status(400)
     .json({ status: 400, message: "we can't handle this request" });
